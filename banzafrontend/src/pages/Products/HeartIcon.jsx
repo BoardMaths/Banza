f35647bf903b6f45410be1578b2a5441677a5d61
BoardMaths <lvic3690@gmail.com>
2024-11-29 07:03:09 +0100

    Add files via upload

---
+++ banzafrontend/src/pages/Products/HeartIcon.jsx
@@ -0,0 +1,51 @@
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addToFavorites,
+  removeFromFavorites,
+  setFavorites,
+} from "../../redux/features/favorites/favoriteSlice";
+import {
+  addFavoriteToLocalStorage,
+  getFavoritesFromLocalStorage,
+  removeFavoriteFromLocalStorage,
+} from "../../Utils/localStorage";
+import { useEffect } from "react";
+
+const HeartIcon = ({ product }) => {
+  const dispatch = useDispatch;
+  const favorites = useSelector((state) => state.favorites) || [];
+  const isFavorite = favorites.some((p) => p._id === product._id);
+
+  useEffect(() => {
+    const favoritesFromLocalStorage = getFavoritesFromLocalStorage();
+    dispatch(setFavorites(favoritesFromLocalStorage));
+  }, []);
+
+  const toggleFavorites = () => {
+    if (isFavorite) {
+      dispatch(removeFromFavorites(product));
+      //remove product from local storage
+      removeFavoriteFromLocalStorage(product._id);
+    } else {
+      dispatch(addToFavorites(product));
+      //add product from local storage
+      addFavoriteToLocalStorage(product);
+    }
+  };
+
+  return (
+    <div
+      onClick={toggleFavorites}
+      className="absolute top-2 right-5 cursor-pointer"
+    >
+      {isFavorite ? (
+        <FaHeart className="text-pink-500" />
+      ) : (
+        <FaRegHeart className="text-white" />
+      )}
+    </div>
+  );
+};
+
+export default HeartIcon;
